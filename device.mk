d5a6a344f26fc0b9ab2e064cc97367e57d0e8f62
itsxrp <itsxrproms@gmail.com>
2023-11-09 06:50:33 +0100

    lunaa: Build SecureElement and NfcNci Packages
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc-service.st \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
+    init.lunaa.rc \
+    SecureElement \
+    NfcNci \
     Tag
 
 PRODUCT_COPY_FILES += \
